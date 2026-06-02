5b4a09d1bf35ab267d5ed13d7889435aa25b4dd9
ZincP <shinlingwoo@gmail.com>
2026-06-02 13:16:38 +0000

    1 renew

--- Problem/UVa_12019.cpp
+++ Problem/UVa_12019.cpp
@@ -1,22 +1,20 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
+int mon[13] = {-1, -4, 0, 0, -3, -5, -1, -3, -6, -2, -4, 0, -2};
+string w[] = {"Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday", "Sunday"};
+ 
 int main() {
-    int t, m, d;
-    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    const char *days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d %d", &m, &d);
-        
-        int total_days = d;
-        for (int i = 1; i < m; i++) {
-            total_days += days_in_month[i];
+    int T, m, d;
+    cin >> T;
+    while (T--) {
+        cin >> m >> d;
+        int delta = (d - mon[m]) % 7;
+        if (delta >= 0){
+            cout << w[delta] << "\n";
+        } else {
+            cout << w[7 + delta] << "\n";
         }
-
-        int result = (total_days + 4) % 7; 
-        
-        printf("%s\n", days[result]);
     }
-    return 0;
 }
